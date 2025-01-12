1c543e15a226dbcf7411ce713ddf5acaa950f070
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-01-12 19:05:22 +0100

    Dodaje plik env

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,12 @@
 import { APP_VERSION } from './version';
 
 const Footer = () => {
+  const VITE_YOU_TOKEN = import.meta.env.VITE_YOU_TOKEN;
+
   return (
     <footer style={{ textAlign: 'center', marginTop: 40 }}>
       <p>Wersja: {APP_VERSION}</p>
+      <p>Token: {VITE_YOU_TOKEN}</p>
     </footer>
   );
 };
